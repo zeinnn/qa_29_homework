491604397921c0ca93f492ccd85e4e5ce75f77f7
zeinnn6387 <smithjohn6387@gmail.com>
2022-05-12 21:26:03 +0300

    First_HW

---
+++ HW1_Console/script.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cd papka_3;
+mkdir papka_7 papka_8 papka_9;
+cd papka_7;
+touch text_4.txt text_5.txt text_6.txt json_3.json json_4.json;
+mkdir papka_10 papka_11 papka_12;
+ls -la;
+mv {text_4.txt,text_5.txt} papka_11/
